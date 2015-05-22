1b333b45f462e8835b4c856e2c1e0994dda1d14b
Andre Hartmann <Andre.Hartmann@zalando.de>
2015-05-22 17:07:29 +0200

    disabled hystrix for db queries

--- src/org/zalando/stups/kio/sql.clj
+++ src/org/zalando/stups/kio/sql.clj
@@ -13,7 +13,7 @@
 ; limitations under the License.
 
 (ns org.zalando.stups.kio.sql
-  (:require [org.zalando.stups.yesql-hystrix :refer [defquerycommands]]
+  (:require [yesql.core :refer [defqueries]]
             [org.zalando.stups.friboo.system.db :refer [def-db-component]]))
 
 (def-db-component DB :auto-migration? true)
@@ -26,9 +26,9 @@
    :db-password    "postgres"
    :db-init-sql    "SET search_path TO zk_data, public"})
 
-(defquerycommands "db/applications.sql")
-(defquerycommands "db/versions.sql")
-(defquerycommands "db/approvals.sql")
+(defqueries "db/applications.sql")
+(defqueries "db/versions.sql")
+(defqueries "db/approvals.sql")
 
 (def column-prefix-pattern #"[a-z]+_(.+)")
 
