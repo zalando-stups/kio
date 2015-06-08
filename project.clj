5b98d425853c452a28b6c00d902960d5f6c79909
Andre Hartmann <Andre.Hartmann@zalando.de>
2015-06-08 17:19:11 +0200

    update dependencies

--- project.clj
+++ project.clj
@@ -7,8 +7,8 @@
 
   :min-lein-version "2.0.0"
 
-  :dependencies [[org.zalando.stups/friboo "0.20.0"]
-                 [yesql "0.5.0-rc2"]]
+  :dependencies [[org.zalando.stups/friboo "0.21.0"]
+                 [yesql "0.5.0-rc3"]]
 
   :main ^:skip-aot org.zalando.stups.kio.core
   :uberjar-name "kio.jar"
