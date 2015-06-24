e2ca04862699b7003220685fa21bbf7d5de39bc9
Nikolaus Piccolotto <prayerslayer@gmail.com>
2015-06-24 11:18:12 +0200

    #8 add db check for positivity
    :sunflower: :sunny: :sunflower:

--- resources/db/migration/V3__Add required_approvers.sql
+++ resources/db/migration/V3__Add required_approvers.sql
@@ -1,3 +1,3 @@
 ALTER TABLE zk_data.version
 -- how many approvers a version needs
-ADD COLUMN a_required_approvers SMALLINT DEFAULT 2;
+ADD COLUMN a_required_approvers SMALLINT DEFAULT 2 CHECK (a_required_approvers > 0);
