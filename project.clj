c73ace35dde3b117ac90ceac81e2dd745b5e2762
Tobias Sarnowski <tobias.sarnowski@zalando.de>
2015-07-14 14:48:56 +0200

    fixed project version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.zalando.stups/kio "0.5.0"
+(defproject org.zalando.stups/kio "0.6.0-SNAPSHOT"
   :description "The application registry."
   :url "https://github.com/zalando-stups/kio"
 
