f066955313dfcb383d99c087386efe68d3dad4d0
Nikolaus Piccolotto <prayerslayer@gmail.com>
2015-07-14 17:23:12 +0200

    #10 remove NOT NULLs as existing rows are NULL

--- resources/db/migration/V5__Add auditing fields.sql
+++ resources/db/migration/V5__Add auditing fields.sql
@@ -3,13 +3,13 @@ ALTER TABLE zk_data.application
 ADD COLUMN a_created TIMESTAMP NOT NULL DEFAULT NOW();
 
 ALTER TABLE zk_data.application
-ADD COLUMN a_created_by TEXT NOT NULL;
+ADD COLUMN a_created_by TEXT;
 
 ALTER TABLE zk_data.application
 ADD COLUMN a_last_modified TIMESTAMP NOT NULL DEFAULT NOW();
 
 ALTER TABLE zk_data.application
-ADD COLUMN a_last_modified_by TEXT NOT NULL;
+ADD COLUMN a_last_modified_by TEXT;
 
 
 -- version auditing
@@ -17,9 +17,9 @@ ALTER TABLE zk_data.version
 ADD_COLUMN v_created TIMESTAMP NOT NULL DEFAULT NOW();
 
 ALTER TABLE zk_data.version
-ADD_COLUMN v_created_by TEXT NOT NULL;
+ADD_COLUMN v_created_by TEXT;
 
 ALTER TABLE zk_data.version
-ADD_COLUMN v_last_modified_by TEXT NOT NULL;
+ADD_COLUMN v_last_modified_by TEXT;
 
 -- last_modified is already there with migration V2
