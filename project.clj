06d151f741086caa0d8fb6d88a3d174e1b9374f9
Tobias Sarnowski <tobias.sarnowski@zalando.de>
2015-07-16 12:22:50 +0200

    upgraded friboo

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
 
   :min-lein-version "2.0.0"
 
-  :dependencies [[org.zalando.stups/friboo "0.22.0"]
+  :dependencies [[org.zalando.stups/friboo "0.26.0"]
                  [yesql "0.5.0-rc3"]]
 
   :main ^:skip-aot org.zalando.stups.kio.core
