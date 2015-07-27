b43c06c5e416ea3137a4db4c4cffe4cbe34292ef
Tobias Sarnowski <tobias.sarnowski@zalando.de>
2015-07-27 12:45:36 +0200

    Version 0.14.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.zalando.stups/kio "0.13.0"
+(defproject org.zalando.stups/kio "0.14.0-SNAPSHOT"
   :description "The application registry."
   :url "https://github.com/zalando-stups/kio"
 
