04038aa6654b34ead237fa0cae8c2adaf0fe17d6
Tobias Sarnowski <tobias.sarnowski@zalando.de>
2015-07-30 09:57:49 +0200

    hopefully fixed tx detection

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
 
   :min-lein-version "2.0.0"
 
-  :dependencies [[org.zalando.stups/friboo "0.31.0"]
+  :dependencies [[org.zalando.stups/friboo "0.32.0"]
                  [yesql "0.5.0-rc3"]]
 
   :main ^:skip-aot org.zalando.stups.kio.core
