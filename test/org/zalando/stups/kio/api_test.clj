6e3a69ce30196bdd1d47fdc0438cae11c91e2c00
Nikolaus Piccolotto <prayerslayer@gmail.com>
2015-07-31 16:25:48 +0200

    #12 first real test

--- test/org/zalando/stups/kio/api_test.clj
+++ test/org/zalando/stups/kio/api_test.clj
@@ -12,3 +12,13 @@
   (with-redefs [sql/cmd-read-application (constantly [{:a_id "kio"}])]
     (is (:id (api/load-application "kio" nil)
         "kio"))))
+
+(deftest test-update-application-criticality1
+  "If the application does not exist, it should not call update and return 404"
+  (let [calls (atom 0)]
+    (with-redefs [api/application-exists? (constantly false)
+                  sql/update-application-criticality! (fn [] (swap! calls inc))]
+      (let [response (api/update-application-criticality! {:application_id "kio"} nil nil)]
+        (is (= @calls 0))
+        (is (= 404
+               (:status response)))))))
