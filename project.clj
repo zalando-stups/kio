ebbf8953e42be068450033f383936c867bc8dad2
Andre Hartmann <andre.hartmann@zalando.de>
2015-09-07 14:44:14 +0200

    Version 0.20.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.zalando.stups/kio "0.20.0-SNAPSHOT"
+(defproject org.zalando.stups/kio "0.20.0"
   :description "The application registry."
   :url "https://github.com/zalando-stups/kio"
 
