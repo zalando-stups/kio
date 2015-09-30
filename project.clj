e06cb19613d46a0b56181ba1997dbb0e26eb90c5
Nikolaus Piccolotto <prayerslayer@gmail.com>
2015-09-30 14:45:17 +0200

    Development version 0.24.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.zalando.stups/kio "0.23.1"
+(defproject org.zalando.stups/kio "0.24.0-SNAPSHOT"
   :description "The application registry."
   :url "https://github.com/zalando-stups/kio"
 
