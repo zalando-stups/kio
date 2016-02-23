8f396d01a21433713e21b1f3d5392b5c9f61cbf3
Henning Jacobs <henning@jacobs1.de>
2016-02-23 12:40:59 +0100

    use latest Friboo lib to support TOKENINFO_URL env var

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
   :min-lein-version "2.0.0"
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.zalando.stups/friboo "1.5.4"]
+                 [org.zalando.stups/friboo "1.6.0"]
                  [clj-time "0.11.0"]
                  [yesql "0.5.1"]]
 
