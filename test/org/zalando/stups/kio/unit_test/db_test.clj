fc22d916854757cfb1cd6843962a451560c9884a
Juho Ylipoti <juho.ylipoti@zalando.de>
2018-08-31 16:52:14 +0200

    mark as integration

--- test/org/zalando/stups/kio/unit_test/db_test.clj
+++ test/org/zalando/stups/kio/unit_test/db_test.clj
@@ -13,7 +13,6 @@
              :password    "postgres"
              :init-sql    "SET search_path TO zk_data, public"})
 
-(deftest apply-migrations
-
+(deftest ^:integration apply-migrations
   (fact "run migrations against db so that docs can be generated"
     (component/stop (org.zalando.stups.friboo.system.db/start-component (sql/map->DB {:configuration config}) true))))
